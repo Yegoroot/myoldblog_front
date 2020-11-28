c396b8a0460a2710c791d80d2fc83325d0cb8ca7
Yegor Kirsanov <kirsanov.yegor@gmail.com>
2020-11-28 04:35:34 +0300

    Dynamic Logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,6 +2,16 @@ import React from 'react'
 import { Link as RouterLink } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core'
 
+import TextTransition, { presets } from 'react-text-transition'
+
+const TEXTS = [
+  'Code',
+  'JS',
+  'Style',
+  'Cs',
+  'Work'
+]
+
 const useStyles = makeStyles((theme) => ({
 
   logo: {
@@ -9,11 +19,23 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.text.primary,
     fontSize: '1.4rem',
     textDecoration: 'none'
+  },
+  logoTitle: {
+    marginRight: 7
   }
 }))
 
 const Logo = () => {
   const classes = useStyles()
+  const [index, setIndex] = React.useState(0)
+
+  React.useEffect(() => {
+    const intervalId = setInterval(() => setIndex((index) => index + 1),
+      4000 // every 3 seconds
+    )
+
+    return clearInterval(intervalId)
+  })
   return (
 
     <RouterLink
@@ -21,12 +43,11 @@ const Logo = () => {
       className={classes.logo}
 
     >
-      I Keep My Code
-      {/* <img
-        alt="Logo"
-        src={`/static/images/logo/dua/${settings.theme}.png`}
-        {...props}
-      /> */}
+      <span className={classes.logoTitle}>I Keep My</span>
+      <TextTransition
+        text={TEXTS[index % TEXTS.length]}
+        springConfig={presets.wobbly}
+      />
     </RouterLink>
   )
 }
